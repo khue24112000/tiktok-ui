9ba3de1e2fb1f3675017340f924138193f6ce8b9
khue24112000 <khue24112000@gmail.com>
2023-03-10 22:15:24 +0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
-import './App.css';
 import Button from './components/Button';
 
 function App() {
-  return (
-    <>
-      <Button />
-      <h1>Hello ae</h1>
-    </>
-  )
+    return (
+        <>
+            <Button />
+            <h1>Hello ae</h1>
+        </>
+    );
 }
 
 export default App;
