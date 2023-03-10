5b0b180a8b8327b4395a8249d18ed35ea8c37bd8
khue24112000 <khue24112000@gmail.com>
2023-03-10 17:57:49 +0700

    customize-cra + babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <>
+      <Button />
       <h1>Hello ae</h1>
     </>
   )
---
+++ src/clickAlert.js
@@ -0,0 +1,5 @@
+const myAlert = () => {
+    alert("helloo ae nhe")
+}
+
+export default myAlert
